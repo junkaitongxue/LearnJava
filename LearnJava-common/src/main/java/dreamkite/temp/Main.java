1478f7597ccf2bd95026202d592fcf6963f7d824
junkaitongxue <252647920@qq.com>
2021-09-22 22:13:49 +0800

    【describe】test ssh

--- LearnJava-common/src/main/java/dreamkite/temp/Main.java
+++ LearnJava-common/src/main/java/dreamkite/temp/Main.java
@@ -4,6 +4,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
     public static void main(String[] args) {
+
         AtomicInteger atomicInteger = new AtomicInteger();
     }
 }
